cb629b49eb43b0939a43f550c695dfce00332bc1
Fábio Picoli <fabiopicolijr@gmail.com>
2021-12-08 22:54:02 -0300

    Binary search

---
+++ src/BinarySearch/binarySearch.js
@@ -0,0 +1,28 @@
+module.exports = {
+  search: function(list, item){
+    let lower = 0;
+    let higher = (list.length - 1);
+    let middle;
+    let guess;
+
+    while(lower <= higher){
+      middle = Math.floor((higher + lower) / 2)
+      guess = list[middle];
+
+      if (guess === item){
+        return middle;
+      }
+
+      if(guess > item){
+        higher = middle - 1;
+      } else {
+        lower = middle + 1;
+      }
+    }
+
+    console.log(middle);
+
+    return 'None';
+  }
+}
+
---
+++ src/BinarySearch/binarySearch.test.js
@@ -0,0 +1,16 @@
+const binarySearch = require('./binarySearch');
+
+describe('Binary Search Tests', () => {
+
+  it('should return None when the number doesnt exists in the list', () => {
+    const list = [1, 2, 3, 4, 5];
+
+    expect(binarySearch.search(list, 8)).toEqual('None');
+  });
+
+  it('should return the position of the item in the array list', () => {
+    const list = [1, 2, 3, 4, 5];
+
+    expect(binarySearch.search(list, 2)).toEqual(1);
+  });
+});
