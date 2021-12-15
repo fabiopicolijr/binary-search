804eba8fee6789678de448083c9ed05c2a0e05ec
Fábio Picoli <fabiopicolijr@gmail.com>
2021-12-15 07:06:17 -0300

    ways of import

--- src/BinarySearch/binarySearch.js
+++ src/BinarySearch/binarySearch.js
@@ -1,26 +1,24 @@
-module.exports = {
-  search: function(list, item){
-    let lower = 0;
-    let higher = (list.length - 1);
-    let middle;
-    let guess;
+module.exports.binarySearch = (list, item) => {
+  let lower = 0;
+  let higher = (list.length - 1);
+  let middle;
+  let guess;
 
-    while(lower <= higher){
-      middle = Math.floor((higher + lower) / 2)
-      guess = list[middle];
+  while(lower <= higher){
+    middle = Math.floor((higher + lower) / 2)
+    guess = list[middle];
 
-      if (guess === item){
-        return middle;
-      }
+    if (guess === item){
+      return middle;
+    }
 
-      if(guess > item){
-        higher = middle - 1;
-      } else {
-        lower = middle + 1;
-      }
-    }    
+    if(guess > item){
+      higher = middle - 1;
+    } else {
+      lower = middle + 1;
+    }
+  }    
 
-    return 'None';
-  }
+  return 'None';
 }
 
--- src/BinarySearch/binarySearch.test.js
+++ src/BinarySearch/binarySearch.test.js
@@ -1,17 +1,19 @@
-const binarySearch = require('./binarySearch');
+const { binarySearch } = require('./binarySearch');
+
+console.log({binarySearch});
 
 describe('Binary Search Tests', () => {
 
   it('should return None when the number doesnt exists in the list', () => {
     const list = [1, 2, 3, 4, 5];
 
-    expect(binarySearch.search(list, 8)).toEqual('None');
+    expect(binarySearch(list, 8)).toEqual('None');
   });
 
   it('should return the position of the item in the array list', () => {
-    const list = [1, 2, 3, 4, 5];
+    const list = [1, 2, 3, 4, 5];    
 
-    expect(binarySearch.search(list, 2)).toEqual(1);
+    expect(binarySearch(list, 5)).toEqual(4);
   });
   
 });
